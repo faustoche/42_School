437dfdea9e9c3bc2a40299631e52378013d063c1
faustoche <faustine.crocq@gmail.com>
2025-06-22 19:47:26 +0200

    diamond trap

--- 4.5 - CPP03/ex03/DiamondTrap.hpp
+++ 4.5 - CPP03/ex03/DiamondTrap.hpp
@@ -4,8 +4,20 @@
 /*-------------- INCLUDES --------------*/
 
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 /*-------------- CLASS --------------*/
  
+class DiamondTrap : public ScavTrap, public FragTrap
+{
+private:
+	std::string name;
+public:
+	DiamondTrap(std::string name);
+	~DiamondTrap();
+	DiamondTrap(const DiamondTrap &other);
+	DiamondTrap &operator=(const DiamondTrap &other);
+};
 
 #endif
