4617ffce57e550d62dc65a47570fd88bee4b624d
faustoche <faustine.crocq@gmail.com>
2025-07-03 21:23:36 +0200

    cpp05

--- 5.1 - CPP05/ex00/includes/Bureaucrat.hpp
+++ 5.1 - CPP05/ex00/includes/Bureaucrat.hpp
@@ -6,21 +6,53 @@
 #include <iomanip>
 
 /*-------------- CLASS --------------*/
+
 class Bureaucrat
 {
 private:
-	std::string name;
-	int	grade; // from 0 to 150
+	const std::string _name;
+	int	_grade;
 
 public:
 	Bureaucrat();
+	Bureaucrat(const std::string &name, int grade);
 	Bureaucrat(const Bureaucrat &other);
-	Bureaucrat &operator=(Bureaucrat &other);
+	Bureaucrat &operator=(const Bureaucrat &other);
 	~Bureaucrat();
 
-	std::string	&getName() const;
-	int	&getGrade() const;
-	// member function to increment
-	// member function de decrement
+	class GradeTooHighException : public exception
+	{
+	public:
+		GradeTooHighException();
+		GradeTooHighException(const GradeTooHighException &other);
+		GradeTooHighException &operator=(const GradeTooHighException &other);
+		~GradeTooHighException();
+	};
+
+	class GradeTooLowException : public exception
+	{
+	public:
+		GradeTooLowException();
+		GradeTooLowException(const GradeTooLowException &other);
+		GradeTooLowException &operator=(const GradeTooLowException &other);
+		~GradeTooLowException();
+	};
+
+	std::string	getName() const;
+	int	getGrade() const;
+	void incrementGrade(); // augmente grade donc diminu numero
+	void decrementeGrade(); // diminue grade donc augmente numero
 
 };
+
+class exception
+{
+public:
+	exception() throw();
+	exception (const exception&) throw();
+	exception &operator=(const exception&) throw();
+	virtual ~exception() throw();
+	virtual const char *what() const throw();
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &f);
--- 5.1 - CPP05/ex00/src/Bureaucrat.cpp
+++ 5.1 - CPP05/ex00/src/Bureaucrat.cpp
@@ -1,11 +1,51 @@
-#include "Bureaucrat.hpp"
+#include "../includes/Bureaucrat.hpp"
 
 /*-------------- CONSTRUCTORS --------------*/
 
+Bureaucrat::Bureaucrat() : _name("unnamed"){
+	_grade = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name (name) {
+	if (grade < 1)
+		throw (GradeTooHighException());
+	if (grade > 150)
+		throw (GradeTooLowException());
+	_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other){
+	*this = other;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other){
+	if (this != &other){
+		this->_name = other._name;
+		this->_grade = other._grade;
+	}
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat(){
+	std::cout << "Destructor called" << std::endl;
+}
 
 /*-------------- GETTERS --------------*/
 
+std::string Bureaucrat::getName() const{
+	return (this->_name);
+}
 
+int	Bureaucrat::getGrade() const{
+	return (this->_grade);
+}
 
 /*-------------- FUNCTIONS --------------*/
 
+
+
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &f) {
+	return (out);
+}
